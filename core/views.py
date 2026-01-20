0e413b57a0d08f41ec257138245293ec6250687b
jadav <jadavdhiraj020@gmail.com>
2026-01-20 15:03:08 +0530

    Added template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def add(a:int, b:int):
     return a+b
+
+
+
+def index(request):
+    return HttpResponse("Hello, World! This is a simple HTTP response from Django.")
