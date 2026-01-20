3355e4d993976b010249c0b685fb1527084d9888
jadav <jadavdhiraj020@gmail.com>
2026-01-20 16:13:29 +0530

    Add approval-based production deploy

--- core/views.py
+++ core/views.py
@@ -7,4 +7,4 @@ def add(a:int, b:int):
 
 
 def home(request):
-    return HttpResponse("🎉 Django CI/CD Deployment Successful!")
+    return HttpResponse("<h1>Django CI/CD Deployment Successful!</h1><br><h2>Hello Dhiraj</h2>")
